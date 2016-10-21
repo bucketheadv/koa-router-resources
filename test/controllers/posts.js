f19e93a00b00b571ba64ec347de3f6f5a520895b
sven <liuqinglin@eyes-control.com>
2016-10-21 11:55:33 +0800

    bug fixed and finish test

---
+++ test/controllers/posts.js
@@ -0,0 +1,33 @@
+module.exports = {
+  index: function(ctx) {
+    ctx.body = 'all users';
+  },
+
+  new: function(ctx) {
+    ctx.body = 'new user';
+  },
+
+  create: function(ctx) {
+    ctx.body = 'create user';
+  },
+
+  show: function(ctx) {
+    ctx.body = 'show user';
+  },
+
+  edit: function(ctx) {
+    ctx.body = 'edit user';
+  },
+
+  update: function(ctx) {
+    ctx.body = 'update user';
+  },
+
+  destroy: function(ctx) {
+    ctx.body = 'destroy user'
+  },
+
+  test1: function(ctx) {
+    ctx.body = 'test1';
+  }
+}
--- test/resources.js
+++ test/resources.js
@@ -23,5 +23,86 @@ describe('Resources', function() {
       expect(routes).to.includes('/users');
       expect(routes).to.not.includes('/users/new');
     });
+
+    it('should have the only routes with operation `except`', function() {
+      this._router.resources('users', { except: ['new'] });
+      const routes = this._router._router.stack.map(x => x.path);
+      expect(routes).to.not.includes('/users/new');
+    });
+
+    it('should have the only routes with operation `as`', function() {
+      this._router.resources('users', { as: '/admin/users' });
+      const routes = this._router._router.stack.map(x => x.path);
+      expect(routes).to.includes('/admin/users/new');
+    });
+
+    it('should have sub resources', function() {
+      var self = this;
+      this._router.resources('users', { as: '/admin/users'}, function() {
+        this.resources('posts', { as: 'articles' }, function() {
+          this.resources('comments', { as: 'coms', except: ['new'] }, function() {
+            const routes = self._router._router.stack.map(x => x.path);
+            expect(routes).to.includes('/admin/users/:user_id/articles/:article_id/coms');
+            expect(routes).to.not.includes('/admin/users/:user_id/articles/:article_id/coms/new');
+          });
+        });
+        const routes = self._router._router.stack.map(x => x.path);
+        expect(routes).to.includes('/admin/users/:user_id/articles');
+      });
+    });
+  });
+
+  describe('test#collection', function() {
+    beforeEach(function() {
+      this._router = new Router(clone(router));
+      let controllersPath = path.resolve(__dirname, 'controllers');
+      this._router.configControllersPath(controllersPath);
+    });
+    it('should have collection routes', function() {
+      var self = this;
+      this._router.resources('users', {}, function() {
+        this.resources('posts', {}, function() {
+          this.resources('comments', {}, function() {
+            this.collections([
+              { method: 'get', path: 'test2' }
+            ]);
+            const routes = self._router._router.stack.map(x => x.path);
+            expect(routes).to.includes('/users/:user_id/posts/:post_id/comments/test2');
+          });
+          this.collections([
+            { method: 'get', path: 'test1' }
+          ]);
+          const routes = self._router._router.stack.map(x => x.path);
+          expect(routes).to.includes('/users/:user_id/posts/test1');
+        });
+      })
+    });
+  });
+
+  describe("test#members", function() {
+    beforeEach(function() {
+      this._router = new Router(clone(router));
+      let controllersPath = path.resolve(__dirname, 'controllers');
+      this._router.configControllersPath(controllersPath);
+    });
+    it('should have member routes', function() {
+      var self = this;
+      this._router.resources('users', {}, function() {
+        this.resources('posts', {}, function() {
+          this.resources('comments', {}, function() {
+            this.members([
+              { method: 'get', path: 'test2' }
+            ]);
+            const routes = self._router._router.stack.map(x => x.path);
+            expect(routes).to.includes('/users/:user_id/posts/:post_id/comments/:comment_id/test2');
+          });
+          this.members([
+            { method: 'get', path: 'test1'}
+          ]);
+          const routes = self._router._router.stack.map(x => x.path);
+          expect(routes).to.includes('/users/:user_id/posts/:post_id/test1');
+        });
+      });
+    });
   });
 });
